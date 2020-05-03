520a545e35216b6747603069be7ceb1ed36db36d
Andreykin Sergey <rocki67@me.com>
2020-05-04 01:03:37 +0300

    Application model v.0.1

---
+++ app/app.go
@@ -0,0 +1,14 @@
+package app
+
+import "vengeful_replacer/app/scanner"
+
+type Application struct {
+	scanner scanner.Scanner
+	//algorithm
+	//output
+}
+
+func New() (Application, error) {
+
+	return Application{}, nil
+}
