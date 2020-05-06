6d82fb08fa87bd187fbdd8a479942d828be8ccf0
Andreykin Sergey <rocki67@me.com>
2020-05-06 20:32:20 +0300

    Implementation of EmptyAlgorithm

--- app/algorithms/empty_algorithm.go
+++ app/algorithms/empty_algorithm.go
@@ -4,5 +4,20 @@ import "vengeful_replacer/app/dictionaries"
 
 type EmptyAlgorithm struct {
 	data       string
+	result     string
 	dictionary dictionaries.Dictionary
+}
+
+func (alg *EmptyAlgorithm) Run() {
+	alg.result = alg.data
+}
+
+func (alg EmptyAlgorithm) GetResult() string {
+	return alg.result
+}
+
+func New(data string) EmptyAlgorithm {
+	newEntity := EmptyAlgorithm{data: data}
+	newEntity.Run()
+	return newEntity
 }
