58435ffd6793f77523975a696647fa33c44585b4
Andreykin Sergey <rocki67@me.com>
2020-05-10 19:23:56 +0300

    Empty algorithm implementation

--- app/algorithms/empty_algorithm.go
+++ app/algorithms/empty_algorithm.go
@@ -1,6 +1,8 @@
 package algorithms
 
-import "vengeful_replacer/app/dictionaries"
+import (
+	"vengeful_replacer/app/dictionaries"
+)
 
 type EmptyAlgorithm struct {
 	data       string
@@ -9,15 +11,52 @@ type EmptyAlgorithm struct {
 }
 
 func (alg *EmptyAlgorithm) Run() {
-	alg.result = alg.data
+
+	var result []rune
+	runeString := []rune(alg.data)
+
+	for _, letter := range runeString {
+
+		replacer := changeChar(letter, alg.dictionary)
+
+		if replacer == 0 {
+			replacer = letter
+		}
+
+		result = append(result, replacer)
+
+	}
+
+	alg.result = string(result)
+}
+
+func changeChar(runeChar rune, dict dictionaries.Dictionary) rune {
+
+	var result rune
+
+	for origin, replacer := range dict.GetMap() {
+
+		originRune := []rune(origin)[0]
+		originReplacer := []rune(replacer)[0]
+
+		if originRune == runeChar {
+			result = originReplacer
+		}
+	}
+
+	if &result != nil {
+		return result
+	}
+
+	return 0
 }
 
 func (alg EmptyAlgorithm) GetResult() string {
 	return alg.result
 }
 
-func NewEmptyAlgorithm(data string) EmptyAlgorithm {
-	newEntity := EmptyAlgorithm{data: data}
+func NewEmptyAlgorithm(data string, dictionary dictionaries.Dictionary) EmptyAlgorithm {
+	newEntity := EmptyAlgorithm{data: data, dictionary: dictionary}
 	newEntity.Run()
 	return newEntity
 }
