fee879e6cd3e36a4e926ec5adc93f1f595e08390
Andreykin Sergey <rocki67@me.com>
2020-05-17 20:22:43 +0300

    New methods of algorithm interface

--- app/algorithms/algorithm.go
+++ app/algorithms/algorithm.go
@@ -1,6 +1,10 @@
 package algorithms
 
+import "vengeful_replacer/app/dictionaries"
+
 type Algorithm interface {
 	Run()
 	GetResult() string
+	SetDictionary(dictionary dictionaries.Dictionary)
+	SetData(string)
 }
